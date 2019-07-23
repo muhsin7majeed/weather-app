6b0e69eeb22b81560b8f368befa57a9ea2fdb5a9
Muhsin <muhsin7ak@gmail.com>
2019-07-23 19:47:58 +0530

    Updates

---
+++ precache-manifest.800622d0402c711a0a90da4b480f25e2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e156f83b9d64c231a139d4bd8ddf9a00",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "47a02ee1abc754367f27",
+    "url": "/weather-app/static/css/main.1e6e6c49.chunk.css"
+  },
+  {
+    "revision": "ae8f0e0b6fe677b98887",
+    "url": "/weather-app/static/js/2.06974016.chunk.js"
+  },
+  {
+    "revision": "47a02ee1abc754367f27",
+    "url": "/weather-app/static/js/main.458f30f5.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "49d27cc7feb4f849fb3d4134590e977d",
+    "url": "/weather-app/static/media/clear-day.49d27cc7.gif"
+  },
+  {
+    "revision": "222d4ebdfe97d1b9207e0d561916b6cb",
+    "url": "/weather-app/static/media/clear-night.222d4ebd.gif"
+  },
+  {
+    "revision": "b6903f6cafe1b64f196bc6c1a98671f3",
+    "url": "/weather-app/static/media/clouds.b6903f6c.gif"
+  },
+  {
+    "revision": "a64a02121031bec912c4de05170114f5",
+    "url": "/weather-app/static/media/default.a64a0212.jpg"
+  },
+  {
+    "revision": "b760f2ce4b25177a19b0ab301880a34c",
+    "url": "/weather-app/static/media/fog.b760f2ce.gif"
+  },
+  {
+    "revision": "b30bba5b96f5e6c7e561cb3fc1ca94f5",
+    "url": "/weather-app/static/media/partly-cloudy-day.b30bba5b.gif"
+  },
+  {
+    "revision": "99cb646deca8eaf75f2c81f698fef823",
+    "url": "/weather-app/static/media/partly-cloudy-night.99cb646d.gif"
+  },
+  {
+    "revision": "c1a93a1d0de0059a3119b8b1a55ce582",
+    "url": "/weather-app/static/media/rain.c1a93a1d.gif"
+  },
+  {
+    "revision": "78a2a10a3e36b36e2e9108e7baec37ec",
+    "url": "/weather-app/static/media/sleet.78a2a10a.gif"
+  },
+  {
+    "revision": "cb5d8d80f81bd8b6f07d8da128f3ff3e",
+    "url": "/weather-app/static/media/snow.cb5d8d80.gif"
+  },
+  {
+    "revision": "92819ee7a62cd7dacc82aead366028dd",
+    "url": "/weather-app/static/media/wind.92819ee7.gif"
+  }
+]);
